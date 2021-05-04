dcf92ca56d2131709b91c76cb58df3c6be723de7
Jeremiah Odige <jodige6@gmail.com>
2021-05-04 11:55:34 -0400

    4900 Project Files

---
+++ frontend/src/components/Footer.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import { Container, Row, Col } from 'react-bootstrap';
+
+const Footer = () => {
+  return (
+    <footer>
+      <Container>
+        <Row>
+        <Col className='text-center py-3'>
+          CISC 4900 Project By Jeremiah Odige
+        </Col>
+        </Row>
+      </Container>
+    </footer>
+  )
+}
+
+export default Footer
---
+++ frontend/src/components/Meta.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import { Helmet } from 'react-helmet'
+
+const Meta = ({ title, description, keywords }) => {
+  return (
+    <Helmet>
+      <title>{title}</title>
+      <meta name='description' content={description} />
+      <meta name='keyword' content={keywords} />
+    </Helmet>
+  )
+}
+
+Meta.defaultProps = {
+  title: '4900 Project',
+  description: 'E-commerce Store With Cheap Products',
+  keywords: 'electronics, buy electronics, cheap electroincs',
+}
+
+export default Meta
